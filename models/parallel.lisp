8a899cecf1702a0e6ced40418bf7e09847bf422f
Flávio Cruz <flaviocruz@gmail.com>
2012-06-22 21:55:23 +0100

    Add write-string to model.

--- models/parallel.lisp
+++ models/parallel.lisp
@@ -4,4 +4,4 @@
 (deftuple setprio (:type-addr :type-int) :action :linear)
 (deftuple setcolor (:type-addr :type-int :type-int :type-int) :action :linear)
 (deftuple setedgelabel (:type-addr :type-addr :type-string) :action :linear)
-;(deftuple write-string (:type-addr :type-string) :action)
+(deftuple write-string (:type-addr :type-string) :action :linear)
