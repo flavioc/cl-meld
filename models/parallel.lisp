7a180b9e4616d84a12d97cfafb5a744dfb17cf5c
Flávio Cruz <flaviocruz@gmail.com>
2012-10-07 18:32:17 -0400

    Change setprio to set-priority and add add-priority.

--- models/parallel.lisp
+++ models/parallel.lisp
@@ -1,7 +1,8 @@
 (in-package :cl-meld)
 
 (deftuple _init (:type-addr) :init-tuple)
-(deftuple setprio (:type-addr :type-int) :action :linear)
+(deftuple set-priority (:type-addr :type-int) :action :linear)
 (deftuple setcolor (:type-addr :type-int :type-int :type-int) :action :linear)
 (deftuple setedgelabel (:type-addr :type-addr :type-string) :action :linear)
 (deftuple write-string (:type-addr :type-string) :action :linear)
+(deftuple add-priority (:type-addr :type-int) :action :linear)
