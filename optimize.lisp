d2a462a5592099a98bf90d6a92b825a3ee2dc0af
Flávio Cruz <flaviocruz@gmail.com>
2013-12-27 01:46:24 +0000

    Fix broken optimization module for init rule.

--- optimize.lisp
+++ optimize.lisp
@@ -1,24 +1,32 @@
 
 (in-package :cl-meld)
+
+(defun matches-mv-host-to-reg-p (instr)
+	(eq (first instr) :move-host-id-to-reg))
          
-(defun matches-op-host-p (instr)
-   (and (vm-op-p instr)
+(defun matches-op-addr-equal-p (instr)
+   (and (eq (first instr) :addr-equal)
         (reg-p (vm-op-dest instr))
-        (reg-eq-p (vm-op-dest instr) (make-reg 0))
-        (vm-host-id-p (vm-op-v1 instr))
-        (vm-addr-p (vm-op-v2 instr))
-        (op-eq-p (vm-op-op instr) :addr-equal)))
+        (reg-eq-p (vm-op-dest instr) (make-reg 1))
+        (reg-p (vm-op-v1 instr))
+        (reg-p (vm-op-v2 instr))
+		  (reg-eq-p (vm-op-v1 instr) (make-reg 0))
+		  (reg-eq-p (vm-op-v2 instr) (make-reg 1))))
+		
+(defun matches-mv-addr-reg-p (instr)
+	(and (eq (first instr) :move-addr-to-reg)
+		  (reg-eq-p (move-to instr) (make-reg 1))))
         
-(defun matches-op-if-0-p (instr)
+(defun matches-op-if-1-p (instr)
    (and (vm-if-p instr)
-        (reg-eq-p (vm-if-reg instr) (make-reg 0))))
+        (reg-eq-p (vm-if-reg instr) (make-reg 1))))
         
 (defun add-instrs-to-node (hash node instrs)
    (multiple-value-bind (other-instrs found-p) (gethash node hash)
       (declare (ignore found-p))
       (setf (gethash node hash) (append other-instrs instrs))))
 
-(defun get-target-node (op-instr) (addr-num (vm-op-v2 op-instr)))
+(defun get-target-node (op-instr) (addr-num (move-from op-instr)))
 
 (defun select-node-init (start-instrs)
    (declare (optimize (speed 3) (safety 0)))
@@ -28,12 +36,14 @@
          (current start-instrs))
       (loop while (not (null current))
             do (cond
-                  ((at-least-n-p current 2)
+                  ((at-least-n-p current 4)
                      (cond
-                        ((and (matches-op-host-p (nth 0 current))
-                              (matches-op-if-0-p (nth 1 current)))
-                           (add-instrs-to-node hash (get-target-node (nth 0 current)) (vm-if-instrs (nth 1 current)))
-                           (setf current (drop-first-n current 2)))
+                        ((and (matches-mv-host-to-reg-p (nth 0 current))
+										(matches-mv-addr-reg-p (nth 1 current))
+										(matches-op-addr-equal-p (nth 2 current))
+										(matches-op-if-1-p (nth 3 current)))
+                           (add-instrs-to-node hash (get-target-node (nth 1 current)) (vm-if-instrs (nth 3 current)))
+                           (setf current (drop-first-n current 4)))
                         (new-start
                            (setf (cdr ptr) current
                                  ptr current
@@ -56,8 +66,12 @@
             
 (defun make-vm-select-with-rules (hash)
    (letret (instr (make-vm-select-node))
-      (iterate-hash (hash node instrs)
-         (vm-select-node-push instr node instrs))))
+		(set-vm-select-hash instr hash)))
+		
+(defun merge-select-instrs (new old)
+	(if (null old)
+		new
+		(merge-vm-select-node new old)))
       
 (defun optimize-init ()
    (let ((def (find-init-predicate *definitions*)))
@@ -74,10 +88,13 @@
 			 (iterate (second (rule-code init)))
 			 (init-code (iterate-instrs iterate)))
 		(assert (not (null init-code)))
-		(multiple-value-bind (hash to-keep) (select-node-init init-code)
+		(multiple-value-bind (hash to-keep1) (select-node-init init-code)
 			(if (= (hash-table-count hash) 0)
-				(setf (iterate-instrs iterate) to-keep)
-				(let ((new-instr (make-vm-select-with-rules hash)))
+				(setf (iterate-instrs iterate) to-keep1)
+				(let* ((new-instr (make-vm-select-with-rules hash))
+					    (old-instr (find-if #'vm-select-node-p to-keep1))
+						 (to-keep (remove old-instr to-keep1)))
+					(setf new-instr (merge-select-instrs new-instr old-instr))
 					(setf (iterate-instrs iterate) `(,(make-vm-remove (make-reg 0)) ,new-instr ,@(remove-if #'(lambda (x) (and (vm-remove-p x) (= 0 (reg-num (vm-remove-reg x))))) to-keep))))))))
                   
 (defmacro iterate-code ((&key instrs proc) &body body)
