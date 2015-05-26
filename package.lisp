2e31db864a10293eff8869648885800ff3827a46
Flavio Cruz <flaviocruz@gmail.com>
2015-05-26 03:11:54 +0100

    Simplify use o snap stanford files. Always create a random state generator for each function.

--- package.lisp
+++ package.lisp
@@ -5,6 +5,7 @@
  (:use :cl :cl-lex :yacc :ieee-floats :cl-csv)
  (:import-from :flexi-streams :make-in-memory-output-stream)
  (:import-from :arnesi :sharpl-reader)
- (:export :comp :meld-compile :meld-compile-exit :meld-compile-list))
+ (:export :comp :meld-compile :meld-compile-exit :meld-compile-list
+          :snap-stanford-dump))
 
 (arnesi:enable-sharp-l)
--- snap-stanford.lisp
+++ snap-stanford.lisp
@@ -1,27 +1,12 @@
 (in-package :cl-meld)
 
-(defparameter *snap-id* 0)
-
-(defun map-snap-node (id ids)
-   (multiple-value-bind (val found-p) (gethash id ids)
-      (if found-p
-       val
-       (progn
-         (setf val *snap-id*)
-         (incf *snap-id*)
-         (setf (gethash id ids) val)
-         val))))
-
 (defclass snap-data ()
    ((edges
      :initarg :edges
      :accessor snap-edges)
     (nodes
      :initarg :nodes
-     :accessor snap-nodes)
-    (rnd
-     :initarg :rand-state
-     :accessor snap-rand-state)))
+     :accessor snap-nodes)))
 
 (defclass snap-search-data (snap-data)
    ((nodes-search
@@ -35,12 +20,13 @@
      :accessor snap-search-id)))
 
 (defun read-snap-file (filename)
-   (let ((*snap-id* 0)
-         (edge-table (make-hash-table))
+   (let ((edge-table (make-hash-table))
          (ids (make-hash-table)))
       (cl-csv:do-csv (row (pathname filename) :separator #\Tab :quote #\#)
-         (let ((node1 (map-snap-node (parse-integer (first row)) ids))
-               (node2 (map-snap-node (parse-integer (second row)) ids)))
+         (let ((node1 (parse-integer (first row)))
+               (node2 (parse-integer (second row))))
+          (setf (gethash node1 ids) node1)
+          (setf (gethash node2 ids) node2)
           (multiple-value-bind (edges found-p) (gethash node1 edge-table)
            (cond
             (found-p
@@ -53,8 +39,7 @@
 
 (defun snap-file-read (filename)
  (multiple-value-bind (ids edge-table) (read-snap-file filename)
-      (make-instance 'snap-data :nodes ids :edges edge-table
-                                 :rand-state (sb-ext:seed-random-state 0))))
+   (make-instance 'snap-data :nodes ids :edges edge-table)))
 
 (defun snap-search-file-read (filename num-searches fraction)
  (multiple-value-bind (ids edge-table) (read-snap-file filename)
@@ -64,7 +49,6 @@
             do (let ((i (random (hash-table-count ids) rnd)))
                   (setf (gethash i source) t)))
       (make-instance 'snap-search-data :nodes ids :edges edge-table
-                                       :rand-state rnd
                                        :fraction fraction
                                        :nodes-search source))))
 
@@ -75,13 +59,31 @@
            do (add-mapping nodes node node))
    nodes))
 
+(defun compute-snap-edge-weight (rnd) (1+ (random 500 rnd)))
+
 (defmethod data-input-node-axioms ((obj snap-data) (n integer))
    (multiple-value-bind (vec found-p) (gethash n (snap-edges obj))
     (unless found-p
      (return-from data-input-node-axioms nil))
-    (loop for other-node across vec
-          collect (make-subgoal "edge" (list (make-addr other-node)
-                (make-float (1+ (random 500 (snap-rand-state obj)))))))))
+    (let ((rnd (sb-ext:seed-random-state 0)))
+       (loop for other-node across vec
+             collect (make-subgoal "edge" (list (make-addr other-node)
+                         (let ((weight (compute-snap-edge-weight rnd)))
+                          (make-float weight))))))))
+
+(defmethod data-input-dump ((obj snap-data) filename)
+ (with-open-file (stream filename :direction :output)
+   (let ((rnd (sb-ext:seed-random-state 0)))
+      (loop for n being the hash-keys of (snap-nodes obj)
+            do (multiple-value-bind (vec found-p) (gethash n (snap-edges obj))
+                  (when vec
+                   (loop for other-node across vec
+                        do (let ((weight (compute-snap-edge-weight rnd)))
+                            (format stream "~a ~a ~a~%" n other-node weight)))))))))
+
+(defun snap-stanford-dump (filename outfile)
+ (let ((snap (snap-file-read filename)))
+   (data-input-dump snap outfile)))
 
 (defun snap-search-build-edges (obj n)
    (multiple-value-bind (vec found-p) (gethash n (snap-edges obj))
@@ -109,19 +111,19 @@
                   n-targets))))
 
 (defmethod data-input-node-axioms ((obj snap-search-data) (n integer))
-   (let ((facts (snap-search-build-edges obj n))
-         (rnd (random 100 (snap-rand-state obj)))
-         (n-nodes (hash-table-count (snap-nodes obj))))
+   (let* ((facts (snap-search-build-edges obj n))
+          (rnd (sb-ext:seed-random-state 0))
+          (n-nodes (hash-table-count (snap-nodes obj))))
     (push (make-subgoal "value" (list (make-int n :type-int))) facts)
     (when (gethash n (snap-search-source-nodes obj))
       (let (targets
             (ls (make-nil))
             (n-targets (truncate (* n-nodes (/ (coerce (snap-search-fraction obj) 'float) 100)))))
        (when (= (mod n 5) 0)
-         (setf targets (snap-search-find-path n (snap-edges obj) n-targets (snap-rand-state obj))))
+         (setf targets (snap-search-find-path n (snap-edges obj) n-targets rnd)))
        (unless targets
           (loop while (< (length targets) n-targets)
-                do (let ((id (random n-nodes (snap-rand-state obj))))
+                do (let ((id (random n-nodes rnd)))
                      (unless (member id targets)
                       (push id targets)))))
        (loop for id in targets
